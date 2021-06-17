f0a272ee534dfa13a42878c4e7f6a1781c07b286
lennox <lennoxnunu@gmail.com>
2021-06-17 20:32:52 +0200

    removed imports

--- http_nodejs/lib/ui/widgets/form_model/form_model.dart
+++ http_nodejs/lib/ui/widgets/form_model/form_model.dart
@@ -1,6 +1,4 @@
-import 'package:http_nodejs/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
 abstract class AuthenticationForm extends FormViewModel {
   @override
